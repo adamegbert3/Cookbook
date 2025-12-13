e9c828efddb10efca8572079589f9f20e7c0d5b3
Adam Egbert <adamegbert@icloud.com>
2025-12-12 23:12:24 -0700

    Changes on HTML, and deleted the recipes except for the JSON file.

--- organizedRecipes/egbert/e_miscellaneous.js
+++
@@ -1,9 +0,0 @@
-export const miscRecipes = [
-  {
-    author: "Sue Egbert",
-    tags: ["Miscellaneous"],
-    name: "Cooked Play Dough",
-    recipeIngredient: ["1 cup flour", "¼ cup salt", "1 teaspoon cream of tartar", "1 cup water", "1 tablespoon oil", "Food coloring", "1 tablespoon vanilla"],
-    recipeInstructions: ["Combine and cook until set. Knead."]
-  }
-];
